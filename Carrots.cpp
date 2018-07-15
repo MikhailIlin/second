74f62abdeb4cca1d928caf9c0e6f7e3973d26ed3
MikhalIlin <pochta_vechna@bk.ru>
2018-07-15 19:34:42 +0300

    My First commit in git

---
+++ Carrots.cpp
@@ -0,0 +1,24 @@
+/*
+ * Carrots.cpp
+ *
+ *  Created on: 15 июл. 2018 г.
+ *      Author: user
+ */
+#include <iostream>
+int main()
+{
+	using namespace std;
+	int carrots;
+
+	carrots = 25;
+	cout << "i have ";
+	cout << carrots;
+	cout << " carrots.";
+	cout << endl;
+	carrots = carrots - 1;
+	cout << "Crunch, crunch. Now I have " << carrots << " carrots." << endl;
+	return 0;
+}
+
+
+
